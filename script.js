26c5449d7733905e0cc4ab04074e62d723b5c9cc
João Augusto <gutopaiva2205@gmail.com>
2026-07-26 22:14:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function enviarWhats(event)  {
+event.preventDefault()
+
+const nome = document.getElementById('nome').value
+const mensagem = document.getElementById('mensagem').value
+
+const telefone = '5515981029491'
+
+const texto = `Olá! Me chamo ${nome}, ${mensagem}`
+
+const msgFormatada = encodeURIComponent(texto)
+
+const url = `https://wa.me/${telefone}?text=${msgFormatada}`
+
+window.open(url,'_blank')
+
+}
